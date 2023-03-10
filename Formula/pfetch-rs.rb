9d93ad6f689c7ce18541bacf911b3f595435b3eb
calexanderberg <carlalexanderberg@gmail.com>
2023-03-10 09:15:09 +0100

    pfetch-rs 2.2.0 (new formula)

---
+++ Formula/pfetch-rs.rb
@@ -0,0 +1,18 @@
+class PfetchRs < Formula
+  desc "Pretty system information tool written in Rust"
+  homepage "https://github.com/Gobidev/pfetch-rs"
+  url "https://github.com/Gobidev/pfetch-rs/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "b21a91a9536ca776c38541e3d79a6c8fab4b522db0ef15353cb11e0833e7d3f7"
+  license "MIT"
+  head "https://github.com/Gobidev/pfetch-rs.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "uptime", shell_output("#{bin}/pfetch-rs")
+  end
+end
